3eff82dac8e575d605960749caf83800319d572f
Andrii Sen <senanven@gmail.com>
2022-02-15 22:33:51 +0200

    add . env

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const users = require('./routes/users');
-const iPaddress = require('./helpers/getIpForSwagger')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const users = require("./routes/users");
+require("dotenv").config();
 
-const swaggerUI = require('swagger-ui-express')
-const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const options = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "REST API",
-            version: "1.0.0",
-            description: "A simle rest API"
-        },
-        servers: [ 
-            {
-                url: "http://localhost:80"
-            },
-            {
-                url: `http://${iPaddress}:80`
-            }
-        ]
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "REST API",
+      version: "1.0.0",
+      description: "A simle rest API",
     },
-    apis: ["./routes/*.js"]
-}
-const specs = swaggerJsDoc(options)
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
+    servers: [
+      {
+        url: "http://localhost:80",
+      },
+      {
+        url: `http://${process.env.API_URL}`,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(express.json());
 app.use(cors());
 
-
-app.use('/user', users)
-
+app.use("/user", users);
 
 app.listen(80, () => {
-    console.log('Server running on port 3001...')
+  console.log("Server running on port 3001...");
 });
-
-console.log(iPaddress)
